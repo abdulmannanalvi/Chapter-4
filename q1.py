2d44b9d993692757c24af575979383d4278cedbf
abdulmannan345 <manialviii134@gmail.com>
2024-07-09 14:51:52 +0500

    first commit

---
+++ q1.py
@@ -0,0 +1,14 @@
+fruits = []
+
+f1 = input("Enter the Fruit Name:")
+fruits.append(f1)
+f1 = input("Enter the Fruit Name:")
+fruits.append(f1)
+f1 = input("Enter the Fruit Name:")
+fruits.append(f1)
+f1 = input("Enter the Fruit Name:")
+fruits.append(f1)
+f1 = input("Enter the Fruit Name:")
+fruits.append(f1)
+
+print(fruits)
---
+++ q2.py
@@ -0,0 +1,4 @@
+std_marks = [23,56,13,98,25,97]
+
+std_marks.sort()
+print(std_marks)
